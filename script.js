1b9df895019ba14c01d0ce3c534a4e812647aa91
Vaibhav Karav <vaibhavkarav99@gmail.com>
2022-05-02 20:07:57 +0530

    learn about map Method in detail

--- script.js
+++ script.js
@@ -86,7 +86,7 @@ displayMovements(account1.movements);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 /*
@@ -195,3 +195,34 @@ currenciesUnique.forEach(function (value, _, map) {
 });
 
 */
+
+//////////////////////////////////////
+// map Method
+//////////////////////////////////////
+
+const eurToUsd = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
+
+const movementsUSD = movements.map((mov) => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDFor = [];
+for (let mov of movements) {
+  movementsUSDFor.push(mov * eurToUsd);
+}
+
+console.log(movementsUSDFor);
+
+const movementsDescription = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+      mov
+    )}.`
+);
+
+console.log(movementsDescription);
