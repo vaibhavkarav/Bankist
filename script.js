cdafe28b0c3f8731111ab35384ecaf5f3d680c42
Vaibhav Karav <vaibhavkarav99@gmail.com>
2022-05-11 22:05:39 +0530

    learn the flat and flatMap methods

--- script.js
+++ script.js
@@ -462,10 +462,9 @@ const accountNew = function (accounts) {
 
 console.log(accountNew(accounts));
 
-*/
 
 //////////////////////////////////////
-// some and every
+// some and every Method
 //////////////////////////////////////
 
 console.log(movements);
@@ -488,3 +487,36 @@ const deposit = (mov) => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+
+*/
+
+//////////////////////////////////////
+// flat and flatMap Method
+//////////////////////////////////////
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// Practical example
+// const accountMovements = accounts.map((acc) => acc.movements);
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+// const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+// console.log(overallBalance);
+
+// Chaining(flat)
+const overallBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+// Chaining(flatMap)
+const overallBalance2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
