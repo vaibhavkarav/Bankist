ad389d46a6b9d4c08178af66d68c156eca5cd0ab
Vaibhav Karav <vaibhavkarav99@gmail.com>
2022-05-13 21:10:53 +0530

    learn ways to create and fill arrays   - new Array() method   - fill() method   - Array.from() method

--- script.js
+++ script.js
@@ -529,38 +529,76 @@ const overallBalance2 = accounts
   .reduce((acc, mov) => acc + mov, 0);
   console.log(overallBalance2);
   
-*/
-
+  
 //////////////////////////////////////
 // Sorting Arrays
 //////////////////////////////////////
-
+  
 // Strings
 const owners = ["Vaibhav", "Kartik", "Preeti", "Akshay"];
 console.log(owners.sort());
 console.log(owners);
-
+  
 // Numbers
 console.log(movements);
-
+  
 // Ascending
 // return < 0, A, B (keep order)(-1)
 // return > 0, B, A (switch order)(1)
-
+  
 // movements.sort((a, b) => {
-//   if (a > b) return 1;
-//   if (a < b) return -1;
-// });
-movements.sort((a, b) => a - b);
-console.log(movements);
+  //   if (a > b) return 1;
+  //   if (a < b) return -1;
+  // });
+  movements.sort((a, b) => a - b);
+  console.log(movements);
+    
+  // Descending
+  // return < 0, B, A (switch order)(1)
+  // return > 0, A, B (keep order)(-1)
+    
+  // movements.sort((a, b) => {
+    //   if (a > b) return -1;
+    //   if (a < b) return 1;
+    // });
+    movements.sort((a, b) => b - a);
+    console.log(movements);
+      
+*/
 
-// Descending
-// return < 0, B, A (switch order)(1)
-// return > 0, A, B (keep order)(-1)
+////////////////////////////////////////////
+// More ways of Creating and Filling Arrays
+////////////////////////////////////////////
 
-// movements.sort((a, b) => {
-//   if (a > b) return -1;
-//   if (a < b) return 1;
-// });
-movements.sort((a, b) => b - a);
-console.log(movements);
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays + fill method
+const x = new Array(7);
+console.log(x); // weird behaviour of new Array when only one argument is passed
+
+// console.log(x.map(() => 5)); // does not work
+// x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr);
+
+// Array.from()
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener("click", function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll(".movements__value"),
+    (el) => Number(el.textContent.replace("€", ""))
+  );
+
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll(".movements__value")]; // have to do mapping separately
+});
